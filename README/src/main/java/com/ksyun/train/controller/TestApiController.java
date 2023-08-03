974f0ed584dd06b12ce95de41755acf41c100a01
libo14 <libo14@kingsoft.com>
2023-08-03 15:43:48 +0800

    feat:添加不同级别日志的测试api

--- README/src/main/java/com/ksyun/train/controller/TestApiController.java
+++ README/src/main/java/com/ksyun/train/controller/TestApiController.java
@@ -16,4 +16,16 @@ public class TestApiController {
         return "hello world";
     }
 
+    @RequestMapping("/1")
+    public Object hello1() {
+        log.info("hello world 1");
+        return "hello world 1";
+    }
+
+    @RequestMapping("/2")
+    public Object hello2() {
+//        log.error("hello world 2");
+        return "hello world 2";
+    }
+
 }
