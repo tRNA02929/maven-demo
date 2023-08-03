8b74a8d60651e6c235c070b06cc632b09e3f206e
libo14 <libo14@kingsoft.com>
2023-08-03 11:47:47 +0800

    add: ➕ 添加SpringBootApplication，测试destroy的使用

---
+++ README/src/main/java/com/ksyun/train/MySpringApplication.java
@@ -0,0 +1,21 @@
+package com.ksyun.train;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MySpringApplication implements DisposableBean {
+
+    static ConfigurableApplicationContext context;
+    public static void main(String[] args) {
+        context = SpringApplication.run(MySpringApplication.class, args);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
