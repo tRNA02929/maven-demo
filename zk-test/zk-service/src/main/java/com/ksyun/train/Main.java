f15a13e88f0a558af4db8a32d5bcfbd3d5e21473
libo14 <libo14@kingsoft.com>
2023-08-09 17:47:43 +0800

    remove:删除不必要的测试

--- zk-test/zk-service/src/main/java/com/ksyun/train/Main.java
+++ zk-test/zk-service/src/main/java/com/ksyun/train/Main.java
@@ -10,21 +10,6 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Properties properties = System.getProperties();
-        properties.load(Main.class.getClassLoader().getResourceAsStream("application.properties"));
-        String address = properties.getProperty("zookeeper.addr");
-
-        ZooKeeper zk = new ZooKeeper(address, 20000, new Watcher() {
-            @Override
-            public void process(WatchedEvent watchedEvent) {
-                String path = watchedEvent.getPath();
-                Watcher.Event.KeeperState state = watchedEvent.getState();
-                Watcher.Event.EventType type = watchedEvent.getType();
-            }
-        });
-        List<String> list = zk.getChildren("/", true);
-        zk.getEphemerals("/");
-
-        System.out.println(list);
+        ;
     }
 }
