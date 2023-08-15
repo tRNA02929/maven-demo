f43b8045d0a03d81ef3f91d7f72c3ddcd8108be5
libo14 <libo14@kingsoft.com>
2023-08-15 18:00:02 +0800

    docs:实现功能骨架

--- thrift-test/src/test/thrift/test.thrift
+++ thrift-test/src/test/thrift/test.thrift
@@ -1,10 +1,35 @@
-namespace java com.ksyun.train.thrift
+namespace java com.ksyun.test.thrift
 
-
-struct HelloMsg {
-    1: string msg;
+// 注册请求
+struct RegisterReq {
+    1: string username,
+    2: string password,
 }
 
+// 登录请求
+struct LoginReq {
+    1: string username,
+    2: string password,
+}
+
+// 登录响应
+struct LoginResp {
+    1: string token,
+}
+
+// 注销请求
+struct LogoutReq {
+    1: string token,
+}
+
+// 注销响应
+struct LogoutResp {
+    1: bool success,
+}
+
+
 service Service {
-    HelloMsg sayHello(),
+    LoginResp login(1: LoginReq req),
+    LogoutResp logout(1: LogoutReq req),
+    bool register(1: RegisterReq req),
 }
