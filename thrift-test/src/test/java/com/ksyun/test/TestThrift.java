1dcc2ea42372b43770da35c12912448274a362b7
libo14 <libo14@kingsoft.com>
2023-08-15 18:00:46 +0800

    test:add test

---
+++ thrift-test/src/test/java/com/ksyun/test/TestThrift.java
@@ -0,0 +1,13 @@
+package com.ksyun.test;
+
+import com.ksyun.test.thrift.LoginResp;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestThrift {
+    @Test
+    public void test() throws Exception {
+        LoginResp resp = null;
+        Assert.assertNull(resp);
+    }
+}
