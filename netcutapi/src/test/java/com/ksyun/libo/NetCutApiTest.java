e842567f799540dd853878e3126a15c0a8f28c1b
libo14 <libo14@kingsoft.com>
2023-08-22 22:08:15 +0800

    add:add test

---
+++ netcutapi/src/test/java/com/ksyun/libo/NetCutApiTest.java
@@ -0,0 +1,19 @@
+package com.ksyun.libo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+public class NetCutApiTest {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public static void main(String[] args) {
+        ;
+    }
+
+    @Test
+    public void testRedis(){
+        ;
+    }
+}
