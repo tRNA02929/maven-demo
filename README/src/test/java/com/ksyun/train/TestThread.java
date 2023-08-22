fcb615e8d829e2ca0a3e9d03b1b6a287c252aeb8
libo14 <libo14@kingsoft.com>
2023-08-22 22:09:06 +0800

    add:add test

---
+++ README/src/test/java/com/ksyun/train/TestThread.java
@@ -0,0 +1,36 @@
+package com.ksyun.train;
+
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(exclude = "age")
+@Builder
+class People implements Comparable<People> {
+    private String name;
+    private int age;
+    private String sex;
+
+    @Override
+    public int compareTo(@NotNull People o) {
+        if(equals(o))
+            return 0;
+        return age - o.age;
+    }
+}
+
+
+public class TestThread {
+    public static void main(String[] args) {
+        ConcurrentSkipListSet<People> set = new ConcurrentSkipListSet<>();
+        set.add(People.builder().age(18).name("t1").sex("1").build());
+        set.add(People.builder().age(19).name("t2   ").sex("1").build());
+        set.add(People.builder().age(18).name("t1").sex("1").build());
+
+        System.out.println(set);
+    }
+}
