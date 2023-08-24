d4583c1ffea507f772336d0d902d3c0fd28fec0c
libo14 <libo14@kingsoft.com>
2023-08-24 16:11:25 +0800

    test: Optional

--- README/src/test/java/com/ksyun/test/StringFormatTest.java
+++ README/src/test/java/com/ksyun/test/StringFormatTest.java
@@ -1,4 +1,15 @@
 package com.ksyun.test;
 
+import java.util.Optional;
+
 public class StringFormatTest {
+
+    public static void main(String[] args) {
+        System.out.println(Optional.ofNullable("1").orElse(f()));
+    }
+
+    public static String f() {
+        System.out.println("f");
+        return "f";
+    }
 }
