656b76f1cbf92f821988e6ba4872ac30af531060
libo14 <libo14@kingsoft.com>
2023-09-05 11:07:57 +0800

    add:add log test

--- README/src/test/java/com/ksyun/test/StringFormatTest.java
+++ README/src/test/java/com/ksyun/test/StringFormatTest.java
@@ -1,4 +1,18 @@
 package com.ksyun.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
 public class StringFormatTest {
+    public static void main(String[] args) {
+        log.error("Hello World!");
+    }
+
+    @Test
+    public void testStringFormat() {
+        String str = "Hello %s!";
+        String result = String.format(str, "World");
+        log.error(result);
+    }
 }
