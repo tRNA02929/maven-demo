3025d8ce06a0aab13a79402b5a03d6c8e93e7325
libo14 <libo14@kingsoft.com>
2023-09-11 13:40:53 +0800

    test:add test perfect array

---
+++ README/src/test/java/com/ksyun/test/PerfectArr.java
@@ -0,0 +1,102 @@
+package com.ksyun.test;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class PerfectArr {
+    static int max = 0;
+    static String result = "";
+    static List<String> resultList = new ArrayList<>();
+    static int n, m;
+    static int[] arr;
+
+    static String[] strings;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(Files.newInputStream(new File("test.txt").toPath()));
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        m = sc.nextInt();
+        strings = new String[m];
+        for (int i = 0; i < m; ++i) {
+            strings[i] = sc.next();
+        }
+        dfs(arr, new boolean[m], "", 0);
+        System.out.println(max);
+        System.out.println(result);
+        check();
+    }
+
+    public static void dfs(int[] arr, boolean[] is, String last, int sum) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < m; ++i) {
+            if (!is[i]) {
+                String now = strings[i];
+                if (now.length() <= last.length() && last.contains(now)) {
+                    is[i] = true;
+                    dfs(arr, is, last, sum + 1);
+                    is[i] = false;
+                } else if (now.length() > last.length() && now.contains(last)) {
+                    is[i] = true;
+                    dfs(change(arr, now.replace(last, "")), is, now, sum + 1);
+                    is[i] = false;
+                } else {
+                    for (int j = 0; j <= last.length(); ++j) {
+                        if (last.length() - j <= now.length() && last.substring(j).equals(now.substring(0, last.length() - j))) {
+                            String temp = now.substring(last.length() - j);
+                            is[i] = true;
+                            dfs(change(arr, temp), is, last + temp, sum + 1);
+                            is[i] = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+//        if (sum == m) {
+//            System.out.println(last);
+//        }
+        if (sum > max) {
+            max = sum;
+            result = last;
+        } else if (sum == max && last.length() < result.length()) {
+            result = last;
+        }
+    }
+
+    public static int[] change(int[] arr, String str) {
+        int[] temp = Arrays.copyOf(arr, arr.length);
+        for (int i = 0; i < str.length(); ++i) {
+            int index = str.charAt(i) - 'a';
+            if (temp[index] < 1)
+                return null;
+            temp[index] -= 1;
+        }
+        return temp;
+    }
+
+    public static void check() {
+        for (int i = 0; i < m; ++i) {
+            if (!result.contains(strings[i]))
+                System.out.println(strings[i]);
+        }
+    }
+
+    @Test
+    public void test() {
+        System.out.println("abc".substring(3));
+    }
+
+}
